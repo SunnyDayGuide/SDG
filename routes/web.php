4a6fcf91079a77002372900a9f52f436bf575ab8
icehouze <icehouze@gmail.com>
2018-10-04 16:14:56 -0400

    Cleaned up routes file

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,21 @@
 
 Auth::routes();
 
+// Home Page route
 Route::get('/', 'PageController@home')->name('home');
 
 // Admin Dashboard Routes
-Route::get('dashboard', 'Admin\DashboardController@index')->name('dashboard');
-
-Route::name('dashboard.')->group(function () {
-	Route::resource('dashboard/markets', 'Admin\MarketController');
-	Route::resource('dashboard/categories', 'Admin\CategoryController')->except(['show']);
-
-	Route::get('dashboard/markets/{market}/category/{category}/edit', 'Admin\MarketController@editMarketCategory')->name('marketCategories.edit');
-	Route::patch('dashboard/markets/{market}/category/{category}', 'Admin\MarketController@updateMarketCategory')->name('marketCategories.update');
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+	Route::get('/', 'Admin\DashboardController@index')->name('dashboard');
+	Route::resource('markets', 'Admin\MarketController');
+	Route::resource('categories', 'Admin\CategoryController')->except(['show']);
+	Route::get('markets/{market}/category/{category}/edit', 'Admin\MarketController@editMarketCategory')->name('marketCategories.edit');
+	Route::patch('markets/{market}/category/{category}', 'Admin\MarketController@updateMarketCategory')->name('marketCategories.update');
 });
 
 // Site Routes
 Route::prefix('{market}')->group(function () {
 	Route::get('/', 'MarketController@show')->name('market.home');
-
     Route::get('articles', 'ArticleController@index')->name('articles');
 	Route::get('articles/{article}', 'ArticleController@show');
 
