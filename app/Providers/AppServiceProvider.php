9ab9c9ee4cf629f26d578eddb4af4d3f9a498f07
icehouze <icehouze@gmail.com>
2018-11-19 12:30:14 -0500

    installed laravel/telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
