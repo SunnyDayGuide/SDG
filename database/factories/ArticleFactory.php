594cfb866f9bb0a7e9f13041835e48324c0f1d79
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-01-18 10:30:33 -0500

    Update ArticleFactory.php

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -8,16 +8,15 @@ $factory->define(App\Article::class, function (Faker $faker) {
     return [
         'market_id' => $faker->numberBetween($min = 1, $max = 11),
         'article_type_id' => $faker->numberBetween($min = 1, $max = 3),
-        'title' => $title,
+        'title' => $faker->sentence,
         'author' => $faker->name,
         'image' => $faker->imageUrl($width = 564, $height = 320, 'cats'),
         'content' => $faker->paragraph,
         'excerpt' => $faker->sentence,
         'rating' => 0,
         'featured' => $faker->boolean($chanceOfGettingTrue = 40),
-        'archived' => false,
-        'slug' => str_slug($title),
-        'published_at' => $faker->dateTime($max = 'now', $timezone = null)
+        'status' => $faker->boolean($chanceOfGettingTrue = 70),
+        'publish_date' => $faker->dateTime($max = 'now', $timezone = null)
     ];
 });
 
