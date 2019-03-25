58b02a2030154332f5ebe21afecfe9ad98571703
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-03-25 15:55:52 -0400

    Location Factory

--- database/factories/AdvertiserFactory.php
+++ database/factories/AdvertiserFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Advertiser::class, function (Faker $faker) {
     return [
         'market_id' => $faker->numberBetween($min = 1, $max = 11),
         'name' => $faker->company,
-        'write_up' => '<p>'. $faker->paragraph($nbSentences = 10, $variableNbSentences = true). '</p>',
+        'write_up' => '<p>'. $faker->realText($maxNbChars = 400, $indexSize = 2). '</p>',
         'toll_free' => $faker->tollFreePhoneNumber,
         'website_url' => $faker->optional()->url,
         'ticket_url' => $faker->optional()->url,
--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -4,6 +4,18 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Location::class, function (Faker $faker) {
     return [
-        //
+        'formatted_address' => $faker->address,
+        'street_number' => $faker->buildingNumber,
+        'route' => $faker->streetName,
+        'address_line_1' => $faker->address,
+        'address_line_2' => $faker->secondaryAddress,
+        'city' => $faker->city,
+        'state' => $faker->stateAbbr,
+        'postal_code' => $faker->postcode,
+        'country' => 'United States',
+        'latitude' => $faker->latitude($min = -90, $max = 90),
+        'longitude' => $faker->longitude($min = -180, $max = 180),
+        'telephone' => $faker->phoneNumber,
+        'advertiser_id' => $faker->numberBetween($min = 1, $max = 100)
     ];
 });
