3b190989db7b18cccbaa11f47d45e8113fea4660
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-04-09 17:08:36 -0400

    Coupon page view tweaks

--- resources/views/coupons/_category.blade.php
+++ resources/views/coupons/_category.blade.php
@@ -1,13 +1,13 @@
 @if(!${ $category }->isEmpty())
-<section class="coupon-section" id="{{ $category }}">
+<section class="coupon-section my-5" id="{{ $category }}">
 	<h2>{{ ucwords($category) }}</h2>
 	<!-- Featured & Premier Advertisers -->
-	<div class="row">
+	<div class="row coupon-cards">
 		<div class="card-deck mx-md-0">	
 			@foreach (${ $category } as $coupon)
-			@foreach($coupon->advertisers->where('level_id', '!=' , 1) as $advertiser)
-			@include('coupons._card')
-			@endforeach
+				@foreach($coupon->advertisers->where('level_id', '!=' , 1) as $advertiser)
+					@include('coupons._card')
+				@endforeach
 			@endforeach
 		</div> <!-- End Card Deck-->
 	</div> <!-- End Row-->
@@ -15,12 +15,12 @@
 	@include('banners._zone1')
 
 	<!-- Standard Advertisers -->
-	<div class="row justify-content-center">
-		<div class="col-8">
+	<div class="row justify-content-center coupon-listings">
+		<div class="col-md-8">
 			@foreach (${ $category } as $coupon)
-			@foreach($coupon->advertisers->where('level_id', 1) as $advertiser)
-			@include('coupons._listing')
-			@endforeach	
+				@foreach($coupon->advertisers->where('level_id', 1) as $advertiser)
+					@include('coupons._listing')
+				@endforeach	
 			@endforeach
 		</div>
 	</div>
--- resources/views/coupons/_listing.blade.php
+++ resources/views/coupons/_listing.blade.php
@@ -1,5 +1,7 @@
 <div class="coupon-list-item">
-	<div class="card-advertiser">{{ $advertiser->name }}</div>
-	<div class="card-offer">{{ $coupon->offer }}</div>
-	<div class="card-suboffer">{{ $coupon->suboffer }}</div>
+	<a href="{{ $advertiser->path() }}" class="text-reset text-decoration-none">
+		<div class="card-advertiser">{{ $advertiser->name }}</div>
+		<div class="card-offer">{{ $coupon->offer }}</div>
+		<div class="card-suboffer">{{ $coupon->suboffer }}</div>
+	</a>
 </div>
