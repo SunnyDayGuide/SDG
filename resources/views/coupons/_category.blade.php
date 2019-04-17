15cf62f60b81ac04452980979fb866dcddceea08
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-04-17 13:49:24 -0400

    Coupon PDF download

--- resources/views/coupons/_category.blade.php
+++ resources/views/coupons/_category.blade.php
@@ -1,6 +1,9 @@
 @if(!${ $category }->isEmpty())
 <section class="coupon-section my-5" id="{{ $category }}">
-	<h2>{{ ucwords($category) }}</h2>
+	<h2>
+		{{ $market->name }} {{ ucwords($category) }} Coupons
+		<small class="text-muted"><a href="{{ url($market->slug.'/pdfs/coupons/'.$category.'.pdf') }}" class="text-reset pdf-download">Download PDF</a></small>
+	</h2>
 	<!-- Featured & Premier Advertisers -->
 	<div class="row coupon-cards">
 		<div class="card-deck mx-md-0">	
