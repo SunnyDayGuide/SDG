7e71873da737eeb1e4b0186b428687ce73e0da3a
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-07-26 17:01:03 -0400

    nova redirect on saves

--- app/Nova/Advertiser.php
+++ app/Nova/Advertiser.php
@@ -77,54 +77,51 @@ class Advertiser extends Resource
             BelongsTo::make('Display Level', 'level', 'App\Nova\Level'),
             Boolean::make('Active')->sortable(),
 
-            Heading::make('Basic Info')->hideFromDetail(),
-            Froala::make('Write Up', 'write_up')
-                ->rules('required'),
+            // Heading::make('Basic Info')->hideFromDetail(),
+            // Froala::make('Write Up', 'write_up')
+            //     ->rules('required'),
 
-            Text::make('Toll Free Phone Number', 'toll_free')->hideFromIndex(),
+            // Text::make('Toll Free Phone Number', 'toll_free')->hideFromIndex(),
 
-            Tags::make('Tags')->hideFromIndex(),
+            // Tags::make('Tags')->hideFromIndex(),
 
-            Heading::make('URLs'),
-            Text::make('Website URL')->rules('nullable', 'url')->hideFromIndex(),
-            Text::make('Ticket URL')->rules('nullable', 'url')->hideFromIndex(),
-            Text::make('Booking URL')->rules('nullable', 'url')->hideFromIndex(),
-            Text::make('Reservation URL')->rules('nullable', 'url')->hideFromIndex(),
+            // Heading::make('URLs'),
+            // Text::make('Website URL')->rules('nullable', 'url')->hideFromIndex(),
+            // Text::make('Ticket URL')->rules('nullable', 'url')->hideFromIndex(),
+            // Text::make('Booking URL')->rules('nullable', 'url')->hideFromIndex(),
+            // Text::make('Reservation URL')->rules('nullable', 'url')->hideFromIndex(),
 
-            Heading::make('Social Media'),
-            Text::make('Facebook')->rules('nullable')->hideFromIndex(),
-            Text::make('Twitter')->rules('nullable')->hideFromIndex(),
-            Text::make('Instagram')->rules('nullable')->hideFromIndex(),
-            Text::make('YouTube', 'youtube')->rules('nullable')->hideFromIndex(),
-            Text::make('Pinterest')->rules('nullable')->hideFromIndex(),
+            // Heading::make('Social Media'),
+            // Text::make('Facebook')->rules('nullable')->hideFromIndex(),
+            // Text::make('Twitter')->rules('nullable')->hideFromIndex(),
+            // Text::make('Instagram')->rules('nullable')->hideFromIndex(),
+            // Text::make('YouTube', 'youtube')->rules('nullable')->hideFromIndex(),
+            // Text::make('Pinterest')->rules('nullable')->hideFromIndex(),
 
-            Heading::make('Images'),
-            // Images::make('Logo', 'logo')
-            //     ->thumbnail('full')
+            // Heading::make('Images'),
+            // BelongsTo::make('Logo')->searchable(), 
+
+            // Images::make('Slider Images', 'slider')
+            //     ->customPropertiesFields([
+            //             Text::make('Credit'),
+            //             Textarea::make('Caption'),
+            //         ])
+            //     ->conversion('full')
+            //     ->conversionOnView('card')
+            //     ->thumbnail('sm-card')
+            //     ->multiple()
+            //     ->fullSize()    
             //     ->hideFromIndex(),
-            BelongsTo::make('Logo')->searchable(), 
 
-            Images::make('Slider Images', 'slider')
-                ->customPropertiesFields([
-                        Text::make('Credit'),
-                        Textarea::make('Caption'),
-                    ])
-                ->conversion('full')
-                ->conversionOnView('card')
-                ->thumbnail('sm-card')
-                ->multiple()
-                ->fullSize()    
-                ->hideFromIndex(),
+            // new Panel('Business Hours', $this->hoursFields()),
 
-            new Panel('Business Hours', $this->hoursFields()),
-
-            HasMany::make('Locations'),
+            // HasMany::make('Locations'),
             MorphToMany::make('Categories'),
-            HasMany::make('Coupons'),
-            HasMany::make('Ads'),
-            HasMany::make('Menus'),
-            HasMany::make('Articles'),
-            HasMany::make('Events'),
+            // HasMany::make('Coupons'),
+            // HasMany::make('Ads'),
+            // HasMany::make('Menus'),
+            // HasMany::make('Articles'),
+            // HasMany::make('Events'),
         ];
     }
 
@@ -174,6 +171,18 @@ protected function hoursFields()
           ])->onlyOnForms(),
     ];
 }
+
+    /**
+     * Return the location to redirect the user after creation.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @param \App\Nova\Resource $resource
+     * @return string
+     */
+    public static function redirectAfterCreate(NovaRequest $request, $resource)
+    {
+        return '/resources/'.static::uriKey().'/'.$resource->getKey().'/attach/categories?viaRelationship=categories&polymorphic=1';
+    }
 
     /**
      * Get the cards available for the request.
--- app/Nova/Resource.php
+++ app/Nova/Resource.php
@@ -72,4 +72,30 @@ abstract class Resource extends NovaResource
         return parent::relatableQuery($request, $query);
     }
 
+    /**
+     * Return the location to redirect the user after creation.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @param \App\Nova\Resource $resource
+     * @return string
+     */
+    public static function redirectAfterCreate(NovaRequest $request, $resource)
+    {
+        // return '/resources/'.static::uriKey().'/'.$resource->getKey();
+        return '/resources/'.static::uriKey();
+    }
+
+    /**
+     * Return the location to redirect the user after update.
+     *
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+     * @param \App\Nova\Resource $resource
+     * @return string
+     */
+    public static function redirectAfterUpdate(NovaRequest $request, $resource)
+    {
+        // return '/resources/'.static::uriKey().'/'.$resource->getKey();
+        return '/resources/'.static::uriKey();
+    }
+
 }
