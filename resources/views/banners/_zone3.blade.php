fb70ab882bfe52bfbbcee9dd9dfd3fa5436acd87
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-08-28 16:51:54 -0400

    banner sizes

---
+++ resources/views/banners/_zone3.blade.php
@@ -0,0 +1,22 @@
+<section class="ads">
+	<div class="row text-center">
+		<div class="col-md-4 mb-3 banner">
+			<img src="http://placehold.it/370x200" alt="advertisement" class="img-fluid">
+			<!-- Revive Adserver Asynchronous JS Tag - Generated with Revive Adserver v4.1.4 -->
+			{{-- <ins data-revive-zoneid="1" data-revive-target="_blank" data-revive-id="c606e94eed023d778c8fb4cfe072e15f"></ins>
+			<script type="application/javascript" async src="//adserver.sunnyday.test/www/delivery/asyncjs.php"></script> --}}
+		</div>
+		<div class="col-md-4 mb-3 banner">
+			<img src="http://placehold.it/370x200" alt="advertisement" class="img-fluid">
+			<!-- Revive Adserver Asynchronous JS Tag - Generated with Revive Adserver v4.1.4 -->
+			{{-- <ins data-revive-zoneid="1" data-revive-target="_blank" data-revive-id="c606e94eed023d778c8fb4cfe072e15f"></ins>
+			<script type="application/javascript" async src="//adserver.sunnyday.test/www/delivery/asyncjs.php"></script> --}}
+		</div>
+		<div class="col-md-4 mb-3 banner">
+			<img src="http://placehold.it/370x200" alt="advertisement" class="img-fluid">
+			<!-- Revive Adserver Asynchronous JS Tag - Generated with Revive Adserver v4.1.4 -->
+			{{-- <ins data-revive-zoneid="1" data-revive-target="_blank" data-revive-id="c606e94eed023d778c8fb4cfe072e15f"></ins>
+			<script type="application/javascript" async src="//adserver.sunnyday.test/www/delivery/asyncjs.php"></script> --}}
+		</div>
+	</div>
+</section>
