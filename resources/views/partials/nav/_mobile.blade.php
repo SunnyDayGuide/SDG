d3ac1112f28f6de411c9435b61a9b1355d23fa87
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-08-28 16:52:48 -0400

    Mobile nav WIP

--- resources/views/partials/nav/_mobile.blade.php
+++ resources/views/partials/nav/_mobile.blade.php
@@ -1,5 +1,13 @@
 <!-- Mobile -->
 <div class="mobile-nav d-md-none">
+    <!-- Mobile Top -->
+    <div class="nav-market">
+        <a class="text-reset" href="{{ route('market.home', $market->slug) }}">
+            Plan your trip to <span class="market-name">{{ $market->name_long }}</span>
+        </a>
+    </div>    
+
+     <!-- Categories -->
     <ul class="navbar-nav menu-group-primary">
         @foreach ($navCategories as $category)
         <li class="nav-item dropdown">
@@ -13,9 +21,23 @@
         </li>
         @endforeach
     </ul>
+
+    <!-- Other Page Links -->
     <ul class="navbar-nav">
         <li class="nav-item no-sub"><a class="nav-link" href="{{ route('coupons', $market) }}">Coupons</a></li>
         <li class="nav-item no-sub"><a class="nav-link" href="{{ route('events', $market) }}">Events</a></li>
         <li class="nav-item no-sub"><a class="nav-link" href="{{ route('articles', $market) }}">Trip Ideas</a></li>
+    </ul>
+
+     <!-- Other Destinations -->
+    <ul class="navbar-nav menu-group-primary">
+        <li class="nav-item dropdown">
+            <a id="marketDropdown" class="nav-link dropdown-toggle menu-item-title" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Other Destinations</a>
+            <div class="dropdown-menu">
+                @foreach ($markets as $navMarket)
+                <a class="dropdown-item subcategory-item" href="{{ route('market.home', $navMarket->slug) }}">{{ $navMarket->name_long }}</a>
+                @endforeach
+            </div>
+        </li>
     </ul>
 </div>
