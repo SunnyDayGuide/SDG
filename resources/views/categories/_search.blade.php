bd48e0861ac98c58f5d9e972f5b5ecf394297040
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-10-11 13:58:41 -0400

    Update _search.blade.php

--- resources/views/categories/_search.blade.php
+++ resources/views/categories/_search.blade.php
@@ -8,26 +8,3 @@
 		</div>
 	</form>
 </div>
-
-
-{{-- <div class="search p-3 p-md-5">
-	<h2 class="mb-3 text-center">Search {{ $page->title }}</h2>
-	<div class="container mx-auto text-center">
-		<form method="GET" action="#">
-			<div class="form-row">
-				<div class="form-group col-md-5">
-				  <select id="category" name="category" class="form-control">
-			        <option value>Category Search</option>
-			        @foreach($subcategories->sortBy('name') as $subcategory)
-					<option value="{{ $subcategory->id }}">{{ $subcategory->name }}</option>
-					@endforeach
-			      </select>
-				</div>
-				<div class="form-group col-md-5">
-				  <input type="text" class="form-control" id="keyword" name="q" placeholder="Search for something...">
-				</div>
-				<div class="form-group col-md-2"><button type="submit" class="btn btn-primary btn-block">Search</button></div>
-				</div>
-		</form>
-	</div>
-</div> --}}
