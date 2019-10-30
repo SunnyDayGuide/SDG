a8e0ebe2f4c182003db17fd0f4ca29eadca1a68b
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-10-30 15:55:54 -0400

    Option for pulling a random image from media collection into a card

--- resources/views/partials/_images.blade.php
+++ resources/views/partials/_images.blade.php
@@ -1,6 +1,11 @@
 @php
 $collectionName = $collectionName ?? 'slider';
 $profile = $profile ?? 'card';
+$random = $random ?? false; // default is false
+
+if($random == true) {
+	$image = $item->getMedia($collectionName)->random();
+} else
 $image = $item->getFirstMedia($collectionName);
 @endphp
 
