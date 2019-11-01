b5f05c68036e8b778c5a71b461b05375149c6440
Sunny Day Guide <sdmedia@sunnydayguide.com>
2019-11-01 16:28:11 -0400

    Update _mobile.blade.php

--- resources/views/partials/nav/_mobile.blade.php
+++ resources/views/partials/nav/_mobile.blade.php
@@ -13,7 +13,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle menu-item-title" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">{{ $category->name }}</a>
             <div class="dropdown-menu">
-                <a class="dropdown-item category-home text-white" href="{{ $market->path().'/'.$category->slug }}"><i class="fas fa-home fa-sm mr-2"></i>{{ $category->name }} home</a>
+                <a class="dropdown-item category-home text-white" href="{{ $market->path().'/'.$category->slug }}"><i class="fas fa-home fa-sm mr-2"></i>{{ $category->name }} Home</a>
                 @foreach ($category->navSubcategories->sortBy('name') as $subcategory)
                 <a class="dropdown-item subcategory-item" href="{{ $market->path().'/'.$category->slug.'/'.$subcategory->slug }}">{{ $subcategory->name }}</a>
                 @endforeach
