283df976eca40367daf7c255bf0b15541b0876a2
Sunny Day Guide <32275067+SunnyDayGuide@users.noreply.github.com>
2020-02-14 11:42:46 -0500

    Install & config spatie/cookie-consent

---
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'This website or its third-party tools use cookies to help provide the best user experience.',
+    'agree' => 'Allow cookies',
+];
---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,11 @@
+<div class="js-cookie-consent cookie-consent fixed-bottom bg-advertiser text-primary py-3 px-5 m-0" role="alert">
+	<div class="container text-center">
+	    <span class="cookie-consent__message">
+	        {!! trans('cookieConsent::texts.message') !!} See our <a href="#" class="alert-link">Cookie Policy</a>.
+	    </span>
+		
+	    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-sm btn-primary font-weight-bold text-uppercase ml-2">
+	        {{ trans('cookieConsent::texts.agree') }}
+	    </button>
+    </div>
+</div>
