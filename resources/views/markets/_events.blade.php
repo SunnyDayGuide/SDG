8cc28edd37070b033263b2409b3a295e58948481
Sunny Day Guide <32275067+SunnyDayGuide@users.noreply.github.com>
2020-02-26 17:26:23 -0500

    Update _events.blade.php

--- resources/views/markets/_events.blade.php
+++ resources/views/markets/_events.blade.php
@@ -1,25 +1,17 @@
 <section class="my-3 my-md-5">
 	<div class="row">
 		<div class="col-md-8 offset-md-2">
-			<h2>Events This Week</h2>
-			<div class="row no-gutters">
-
-				<div class="col-md-8">
-					<div class="row no-gutters">
-						<div class="scrollarea pr-3 mr-3">
-						@foreach($events as $event)
-						@include('events._listing')
-						@endforeach
-						</div>
-					</div>
+			<div class="d-flex justify-content-between border-bottom border-light">
+				<h2>Events This Week</h2>
+				<div class="d-flex align-items-center mr-md-2 mb-2">
+					<a href="{{ route('events', $market) }}" class="btn btn-highlight">See All Events</a>
 				</div>
-
-				<div class="col-md-4 mt-2">
-					<div class="bg-highlight h-100 p-2">
-						<a href="{{ route('events', $market) }}" class="btn btn-primary btn-nav mt-3">See All Events</a>
-					</div>
-				</div>
-
+			</div>
+			
+			<div class="scrollarea pr-3 mr-3 mt-4">
+				@foreach($events as $event)
+				@include('events._listing')
+				@endforeach
 			</div>
 		</div>
 	</div>
