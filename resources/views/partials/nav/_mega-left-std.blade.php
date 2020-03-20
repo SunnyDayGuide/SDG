e6a409c404d659690391756147eb75442d296987
Sunny Day Guide <32275067+SunnyDayGuide@users.noreply.github.com>
2020-03-20 16:13:41 -0400

    Megamenu DONE

---
+++ resources/views/partials/nav/_mega-left-std.blade.php
@@ -0,0 +1,57 @@
+<div class="col-lg-8 col-xl-8">
+	<div class="megamenu-std pt-4 px-2">
+		<div class="row">
+			@foreach ($navCategories as $category)
+			<div class="col-lg-6 megamenu-block">
+				<div class="megamenu-heading">
+					<a href="{{ $market->path().'/'.$category->slug }}">{{ $category->name }}</a>
+				</div>
+				<ul class="list-unstyled subcategories">
+					@foreach ($category->navSubcategories->take(7)->sortBy('name') as $subcategory)
+					{{-- @foreach ($category->navSubcategories->sortBy('name') as $subcategory) --}}
+					<li class="nav-item subcategory"><a href="{{ $market->path().'/'.$category->slug.'/'.$subcategory->slug }}" class="nav-link text-small pb-0">{{ $subcategory->name }}</a></li>
+					@endforeach
+					<li class="nav-item subcategory"><a href="{{ $market->path().'/'.$category->slug }}" class="btn-nav btn btn-sm btn-light text-white ml-1 mt-2 more">More</a></li>
+				</ul>
+			</div>
+			@endforeach
+
+			<div class="col-lg-6 megamenu-block megamenu-guide">
+				<div class="row">
+					<div class="col-4">
+						<a href="{{ route('vacation-guide.create', $market->slug) }}" class="guide-image">
+							{{ $market->getFirstMedia('cover') }}
+						</a>
+					</div>
+
+					<div class="col-8">
+						<div class="guide-text">
+							<h4 class="guide-link"><a href="{{ route('vacation-guide.create', $market->slug) }}">Free Digital Vacation Guide</a></h4>
+							<p class="nav-item">
+								Bacon ipsum dolor amet jerky ground round short loin. Cow swine pig turkey pork loin tail.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div> <!-- End row -->
+
+		<!-- Bottom left of megamenu -->
+		<div class="row mt-4 megamenu-bottom">
+			<!-- Bottom submenu -->
+			<div class="col-12 d-flex align-items-end pb-5">
+				@if($market->code == 'BR')
+				<h6 class="megamenu-heading mr-5"><a href="{{ $market->path() }}/entertainment-shows#show-schedule">Show Schedule</a></h6>
+				@endif
+				<h6 class="megamenu-heading mr-5"><a href="{{ $market->path() }}/accommodations">Places to Stay</a></h6>
+				<h6 class="megamenu-heading mr-5"><a href="{{ route('visitor-info', $market) }}">Visitor Info</a></h6>
+
+				@unless($market->code == 'BR' || $market->code == 'SM' || $market->code == 'CG')
+				<h6 class="megamenu-heading mr-5"><a href="{{ route('tide-charts', $market) }}">Tide Charts</a></h6>
+				@endunless
+
+			</div>
+		</div> <!-- End row -->
+	</div>
+</div>
--- resources/views/partials/nav/_megamenu.blade.php
+++ resources/views/partials/nav/_megamenu.blade.php
@@ -4,76 +4,11 @@
 		<div class="container">
 			<div class="row bg-white rounded-0 m-0 shadow">
 				<!-- Left Side Of Megamenu -->
-				<div class="col-lg-8 col-xl-8">
-					<div class="pt-4 px-2">
-						<div class="row">
-							@foreach ($navCategories as $category)
-							<div class="col-lg-6 mb-4">
-								<div class="megamenu-heading">
-									<a href="{{ $market->path().'/'.$category->slug }}">{{ $category->name }}</a>
-								</div>
-								<ul class="list-unstyled subcategories">
-									@foreach ($category->navSubcategories->take(7)->sortBy('name') as $subcategory)
-									{{-- @foreach ($category->navSubcategories->sortBy('name') as $subcategory) --}}
-									<li class="nav-item subcategory"><a href="{{ $market->path().'/'.$category->slug.'/'.$subcategory->slug }}" class="nav-link text-small pb-0">{{ $subcategory->name }}</a></li>
-									@endforeach
-									<li class="nav-item subcategory"><a href="{{ $market->path().'/'.$category->slug }}" class="btn-nav btn btn-sm btn-light text-white ml-1 mt-2 more">More</a></li>
-								</ul>
-							</div>
-							@endforeach
-							@if(count($navCategories) < 4)
-							<div class="col-lg-6 mb-4">
-								@if($market->code == 'BR')
-								<div class="megamenu-heading"><a href="{{ $market->path() }}/entertainment-shows#show-schedule">Show Schedule</a></div>
-								@endif
-								<div class="megamenu-heading"><a href="{{ $market->path() }}/accommodations">Places to Stay</a></div>
-								<div class="megamenu-heading"><a href="{{ route('visitor-info', $market) }}">Visitor Info</a></div>
-								@unless($market->code == 'BR' || $market->code == 'SM' || $market->code == 'CG')
-								<div class="megamenu-heading"><a href="{{ route('tide-charts', $market) }}">Tide Charts</a></div>
-								@endunless
-							</div>
-							@endif
-						</div> <!-- End row -->
-
-						<!-- Bottom left of megamenu -->
-						<div class="row mt-4 megamenu-bottom">
-							<!-- Vacation Guide -->
-							<div class="col-lg-9 mt-4 mt-md-0">
-								<div class="row  position-relative">
-									<div class="col">
-										<a href="{{ route('vacation-guide.create', $market->slug) }}" class="nav-guide">
-											{{ $market->getFirstMedia('cover') }}
-										</a>
-									</div>
-
-									<div class="col-6 col-md-9">
-										<div class="ml-3">
-											<h4 class="guide-link"><a href="{{ route('vacation-guide.create', $market->slug) }}">Free Digital Vacation Guide</a></h4>
-											<p class="nav-item">
-												Bacon ipsum dolor amet jerky ground round short loin. Cow swine pig turkey pork loin tail.
-											</p>
-										</div>
-									</div>
-
-								</div>
-							</div>
-
-							<!-- Bottom submenu -->
-							<div class="col-lg-3">
-								@if($market->code == 'BR')
-								<h6 class="megamenu-heading megamenu-subheading"><a href="{{ $market->path() }}/entertainment-shows#show-schedule">Show Schedule</a></h6>
-								@endif
-								<h6 class="megamenu-heading megamenu-subheading"><a href="{{ $market->path() }}/accommodations">Places to Stay</a></h6>
-								<h6 class="megamenu-heading megamenu-subheading"><a href="{{ route('visitor-info', $market) }}">Visitor Info</a></h6>
-								
-								@unless($market->code == 'BR' || $market->code == 'SM' || $market->code == 'CG')
-								<h6 class="megamenu-heading megamenu-subheading"><a href="{{ route('tide-charts', $market) }}">Tide Charts</a></h6>
-								@endunless
-								
-							</div>
-						</div> <!-- End row -->
-					</div>
-				</div>
+				@if(count($navCategories) < 4)
+				@include('partials.nav._mega-left-std')
+				@else
+				@include('partials.nav._mega-left-alt')
+				@endif
 
 				<!-- Right colored block of megamenu -->
 				<div class="col-lg-4 col-xl-4 p-3 d-none d-lg-block bg-highlight">
