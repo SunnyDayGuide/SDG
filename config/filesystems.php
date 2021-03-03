7eaa0a64c16d8474e9d4415f290f74b06e8f3cb8
Sunny Day Guide <32275067+SunnyDayGuide@users.noreply.github.com>
2021-03-03 15:54:50 -0500

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -82,13 +82,13 @@ return [
         'media' => [
             'driver' => 'local',
             'root'   => storage_path('app/public/media'),
-            'url' => env('APP_URL').'/media',
+            'url' => env('APP_URL').'storage/media',
         ],
 
         'trix' => [
             'driver' => 'local',
             'root'   => storage_path('app/public/images/articles'),
-            'url' => env('APP_URL').'/images/articles',
+            'url' => env('APP_URL').'storage/images/articles',
         ],
 
     ],
